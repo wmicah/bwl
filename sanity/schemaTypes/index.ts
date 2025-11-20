03518a93291d1d1aab90e550536b21853d00fbaf
Micah <wmicah56@gmail.com>
2025-11-19 23:57:38 -0500

    studio

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from "sanity"
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+	types: [],
+}
+import { postType } from "./postType"
+
+export const schemaTypes = [postType]
